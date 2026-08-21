6adc6413aa580fab7551a1b6cbdbcd49a211a893
arakilian0 <arakilian0@gmail.com>
2026-08-21 02:38:32 -0500

    refactor(util/__index): easy to import util module

---
+++ util/__index.js
@@ -0,0 +1,13 @@
+import find from './find.js';
+import merge from './merge.js';
+import print from './print.js';
+import read from './read.js';
+
+const util = {
+  find,
+  merge,
+  print,
+  read
+}
+
+export default util;
