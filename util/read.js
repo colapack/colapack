14727c5a92b49a7ca1ba93ea71ba38087b0623ea
arakilian0 <arakilian0@gmail.com>
2026-08-21 00:46:01 -0500

    feat(util/read): read resolved file paths, parse if json

---
+++ util/read.js
@@ -0,0 +1,19 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import print from './print.js'
+
+export default function read(file) {
+  let resolved = path.resolve(process.cwd(), file);
+
+  if (!fs.existsSync(resolved)) {
+    return print.red(`error: ${resolved} does not exist. Returned 'undefined'`);
+  }
+
+  if (path.extname(resolved) === '.json') {
+    try { return JSON.parse(fs.readFileSync(resolved), 'utf8') } 
+    catch(err) { return print.red(err) }
+  } else {
+    try { return fs.readFileSync(resolved, 'utf8') } 
+    catch(err) { return print.red(err) }
+  }
+}
