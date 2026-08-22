3386b601d4ca40c53544a0fc31e97ee1c367723f
arakilian0 <arakilian0@gmail.com>
2026-08-22 01:15:13 -0500

    fix(util): write and colax import

--- util/__index.js
+++ util/__index.js
@@ -2,12 +2,16 @@ import find from './find.js';
 import merge from './merge.js';
 import print from './print.js';
 import read from './read.js';
+import write from './write.js';
+import colax from './colax.js';
 
 const util = {
   find,
   merge,
   print,
-  read
+  read,
+  write,
+  colax
 }
 
 export default util;
