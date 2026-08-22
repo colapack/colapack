490bc0e57f19925fb45ea75057220b72857c72f5
arakilian0 <arakilian0@gmail.com>
2026-08-21 21:20:10 -0500

    fix(util/read): parseJson option handler

--- util/read.js
+++ util/read.js
@@ -2,12 +2,12 @@ import path from 'node:path';
 import fs from 'node:fs';
 import print from './print.js'
 
-export default function read(file) {
+export default function read(file, parseJson=true) {
   let resolved = path.resolve(process.cwd(), file);
 
   if (!fs.existsSync(resolved)) return undefined;
 
-  if (path.extname(resolved) === '.json') {
+  if (path.extname(resolved) === '.json' && parseJson) {
     try { return JSON.parse(fs.readFileSync(resolved), 'utf8') } 
     catch(err) { return print.red(err) }
   } else {
